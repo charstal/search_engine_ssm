d5037acfad6bea871d625c58f653a1650e02470e
easyBritney <18072712667@163.com>
2019-07-07 00:33:11 +0800

    user interaction aspect

--- src/main/java/cn/edu/zucc/caviar/searchengine/core/aspect/InteractionAspect.java
+++ src/main/java/cn/edu/zucc/caviar/searchengine/core/aspect/InteractionAspect.java
@@ -3,6 +3,7 @@ package cn.edu.zucc.caviar.searchengine.core.aspect;
 import cn.edu.zucc.caviar.searchengine.common.kafka.KafkaProducerServer;
 import cn.edu.zucc.caviar.searchengine.common.query.segment.ChineseSegmentation;
 import cn.edu.zucc.caviar.searchengine.common.utils.RedisTest;
+import cn.edu.zucc.caviar.searchengine.core.pojo.Comment;
 import cn.edu.zucc.caviar.searchengine.core.pojo.User;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -31,8 +32,11 @@ public class InteractionAspect {
     public void declearJoinPointExpression(){}
 
 
-    @Pointcut("execution(* cn.edu.zucc.caviar.searchengine.core.controller.NoteController.getNoteByNoteId(..))")
+    @Pointcut("execution(* cn.edu.zucc.caviar.searchengine.core.controller.NoteController.*(..))")
     public void clickNoteJoinPointExpression() { }
+
+    @Pointcut("execution(* cn.edu.zucc.caviar.searchengine.core.controller.UserController.*(..))")
+    public void collectJoinPointExpression() { }
 
     @Autowired
     KafkaProducerServer kafkaProducer;
@@ -55,21 +59,35 @@ public class InteractionAspect {
     public void clickNoteLog(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
+
+        String uid;
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("USER_SESSION");
+
+        if (user == null)
+            return;
+            //uid = session.getId();
+        else
+            uid = String.valueOf(user.getUserId());
+
         if(methodName.equals("getNoteByNoteId")) {
             System.out.println("ASPECT Note click");
             String noteId = (String) args.get(0);
             String type = " 0 ";
-            String uid;
 
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-            HttpSession session = request.getSession();
-            User user = (User) session.getAttribute("USER_SESSION");
-
-            if (user == null)
-                uid = session.getId();
-            else
-                uid = String.valueOf(user.getUserId());
             value = uid + type + noteId;
+            System.out.println(value);
+            Map<String, Object> res = kafkaProducer.sndMesForTemplate(topic, value, ifPartition, partitionNum, role);
+        }
+        else if(methodName.equals("addComment")){
+
+            System.out.println("ASPECT add comment");
+            Comment comment = (Comment) args.get(0);
+            String type = " 2 ";
+
+            value = uid + type + comment.getNoteId()+" "+comment.getScore();
             System.out.println(value);
             Map<String, Object> res = kafkaProducer.sndMesForTemplate(topic, value, ifPartition, partitionNum, role);
         }
@@ -95,7 +113,7 @@ public class InteractionAspect {
             session.setAttribute("LAST_SEARCH_KEYWORD", searchWords);
 
             if(user==null)
-                uid = session.getId();
+                return;//uid = session.getId();
             else
                 uid = String.valueOf(user.getUserId());
             value = uid+type+searchWords;
@@ -107,6 +125,44 @@ public class InteractionAspect {
                 redisTest.zincrby(hotpotTopic, a, 1.0);
             }
 
+        }
+    }
+
+    @Before("collectJoinPointExpression()")
+    public void beforeCollect(JoinPoint joinPoint){
+        String methodName = joinPoint.getSignature().getName();
+        List<Object> args = Arrays.asList(joinPoint.getArgs());
+
+        String uid;
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("USER_SESSION");
+
+        if (user == null)
+            return;
+            //uid = session.getId();
+        else
+            uid = String.valueOf(user.getUserId());
+
+        if(methodName.equals("addCollectNote")) {
+            System.out.println("ASPECT Note collect");
+            String noteId = (String) args.get(1);
+            String type = " 3 ";
+
+            value = uid + type + noteId;
+            System.out.println(value);
+            Map<String, Object> res = kafkaProducer.sndMesForTemplate(topic, value, ifPartition, partitionNum, role);
+        }
+        else if(methodName.equals("addLikeNote")){
+
+            System.out.println("ASPECT Note like");
+            Comment comment = (Comment) args.get(1);
+            String type = " 3 ";
+
+            value = uid + type + comment.getNoteId()+" "+comment.getScore();
+            System.out.println(value);
+            Map<String, Object> res = kafkaProducer.sndMesForTemplate(topic, value, ifPartition, partitionNum, role);
         }
     }
 }
